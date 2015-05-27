780d82501f0dcc380b9f4a89d84168e9740ef87e
Joyce Ding <joyce.shuyu.ding@gmail.com>
2015-05-26 22:47:27 -0700

    HTI Mockup

--- app/router.js
+++ app/router.js
@@ -7,6 +7,6 @@ var Router = Ember.Router.extend({
 
 export default Router.map(function() {
   this.resource('about', {path: '/'});
-  this.resource('projects');
-  this.resource('talks');
+  this.resource('accelerated-classes');
+  this.resource('weekend-classes');
 });
